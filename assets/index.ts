4e10542d6d997c47718f71de9c18171c1b039010
shoaibhajj09 <shoaibhajj09@gmail.com>
2025-04-27 21:58:19 +0300

    Add files via upload

---
+++ assets/index.ts
@@ -0,0 +1,137 @@
+import brainwave from "./brainwave.svg";
+import typeTest from "./type-test.png";
+import minecraft from "./minecraft.png";
+import talky from "./talky.png";
+import check from "./check.svg";
+
+import play from "./play.svg";
+import gradient from "./gradient.png";
+
+import grid from "./grid.png";
+import grid2 from "./grid-2.png";
+import self from "./self.png";
+import self2 from "./face.jpg";
+import check2 from "./check-02.svg";
+import loading1 from "./loading-01.svg";
+import yourlogo from "./yourlogo.svg";
+import homeSmile from "./home-smile.svg";
+import file02 from "./file-02.svg";
+import searchMd from "./search-md.svg";
+import plusSquare from "./plus-square.svg";
+import recording03 from "./recording-03.svg";
+import recording01 from "./recording-01.svg";
+
+import sliders04 from "./sliders-04.svg";
+import loading from "./loading.png";
+
+
+import discord from "./collaboration/discord.png";
+import figma from "./collaboration/figma.png";
+import framer from "./collaboration/framer.png";
+import notion from "./collaboration/notion.png";
+import photoshop from "./collaboration/photoshop.png";
+import protopie from "./collaboration/protopie.png";
+import raindrop from "./collaboration/raindrop.png";
+import slack from "./collaboration/slack.png";
+
+
+
+
+
+
+import discordBlack from "./socials/discord.svg";
+import facebook from "./socials/facebook.svg";
+import instagram from "./socials/instagram.svg";
+import telegram from "./socials/telegram.svg";
+import twitter from "./socials/twitter.svg";
+import css from "./features/css.svg";
+import html from "./features/html.svg";
+import js from "./features/js.svg";
+import next from "./features/next.svg";
+import next2 from "./features/next2.svg";
+import prisma from "./features/prisma.svg";
+import react from "./features/react.svg";
+import redux from "./features/redux.svg";
+import tailwind from "./features/tailwind.svg";
+import ts from "./features/ts.svg";
+import email from "./contact/email.svg";
+
+import linkedin from "./contact/linkedin.svg";
+import github from "./contact/github.svg";
+import whatsapp from "./contact/whatsapp.svg";
+import telegram2 from "./contact/telegram.svg";
+import chess from "./chess.jpg";
+import games from "./games.jpg";
+import football from "./football.jpg";
+import realmadrid from "./realmadrid.png";
+import Madrid from "./Madrid.png";
+import minecraft1 from "./apps/minecraft.ico";
+import speed2 from "./apps/speed2.png";
+import video from "./apps/video.png";
+import brainwaveImage from "./apps/brainwaveImage.png";
+import spacePortfolio from "./apps/spacePortfolio.png";
+
+// import logo from "./logo.png";
+export {
+  minecraft1,
+  brainwaveImage,
+  spacePortfolio,
+  speed2,
+  video,
+  email,
+  // logo,
+  games,
+  Madrid,
+  realmadrid,
+  football,
+  chess,
+  typeTest,
+  minecraft,
+  talky,
+  telegram2,
+  linkedin,
+  github,
+  whatsapp,
+  brainwave,
+  check,
+  check2,
+  loading1,
+  play,
+  gradient,
+  grid,
+  self,
+  self2,
+  grid2,
+  yourlogo,
+  homeSmile,
+  file02,
+  searchMd,
+  plusSquare,
+  recording03,
+  recording01,
+  sliders04,
+  loading,
+  discord,
+  figma,
+  framer,
+  notion,
+  photoshop,
+  protopie,
+  raindrop,
+  slack,
+  discordBlack,
+  facebook,
+  instagram,
+  telegram,
+  twitter,
+  css,
+  html,
+  js,
+  ts,
+  prisma,
+  react,
+  tailwind,
+  redux,
+  next,
+  next2,
+};
